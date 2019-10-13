bceeeee327763460f333fb4e08017000165b3b2a
jf908 <josh@thefindons.com>
2019-10-13 16:12:06 +0100

    Completed lexer gen

--- src/Language/LexerGen.hs
+++ src/Language/LexerGen.hs
@@ -1,25 +1,26 @@
 {-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
 
-import Prelude hiding (unlines, shows, head, foldl)
+import Prelude hiding (shows)
 import Data.Maybe (fromMaybe)
 import Data.Aeson
-import Data.Text
+import Data.Text (Text, append, pack)
 import Data.Map hiding (foldl)
 import qualified Data.ByteString.Lazy as B
 import GHC.Generics
 import Data.Monoid
+import Data.List (intercalate)
 
-type LexerData = Map Text TokenData
+type LexerData = Map String TokenData
 
 data TokenData =
     TokenData {
         lexeme          :: !String,
-        literal         :: Maybe Text,
-        tokenValue      :: Maybe Text,
-        comment         :: Maybe Text,
-        extraFields     :: Maybe Text,
-        showExtraFields :: Maybe Text,
-        showBody        :: Maybe Text
+        literal         :: Maybe String,
+        tokenValue      :: Maybe String,
+        comment         :: Maybe String,
+        extraFields     :: Maybe String,
+        showExtraFields :: Maybe String,
+        showBody        :: Maybe String
     } deriving (Show, Generic)
 
 instance FromJSON TokenData
@@ -40,23 +41,25 @@ main = do
     case d of
         Left err -> putStrLn err
         Right ps -> do
-            start <- B.readFile startFile
-            end <- B.readFile endFile
-            putStrLn $ lexerToText start end (makeLexer d)
+            start <- readFile startFile
+            end <- readFile endFile
+            putStrLn $ lexerToText start end (makeLexer ps)
 
 
-lexerToText :: Text -> Text -> GeneratedLexer -> Text
+lexerToText :: String -> String -> GeneratedLexer -> String
 lexerToText start end lexer =
-    start ++ (lexerToText' lexer) ++ end
+    start ++ (lexerToText' lexer) ++ end ++ "\n}"
         where
-            lexerToText' :: GeneratedLexer -> Text
+            lexerToText' :: GeneratedLexer -> String
             lexerToText' lexer = '\n' : unlines [ unlines (captureLines lexer)
                     , "{"
                     , "data LexemeClass = " ++ intercalate "\n\t\t| " (classes lexer)
                     , "\t deriving (Eq, Show)"
+                    , ""
                     , "mkL :: LexemeClass -> AlexInput -> Int -> Alex Token"
                     , "mkL c (p, _, _, str) len = let t = take len str in case c of"
                     , '\t' : intercalate "\n\t" (classToTokens lexer)
+                    , ""
                     , "alexEOF :: Alex Token"
                     , "alexEOF = return TEoF"
                     , ""
@@ -64,24 +67,28 @@ lexerToText start end lexer =
                     , "-- a monadic parser"
                     , "lexWrap :: (Token -> Alex a) -> Alex a"
                     , "lexWrap = (alexMonadScan >>=)"
+                    , ""
                     , "-- | Type to represent tokens in the output stream"
-                    , "data Token = " ++ intercalate "\n\t\t| " (classes tokens)
+                    , "data Token = " ++ intercalate "\n\t\t| " (tokens lexer)
+                    , "\t| TEoF -- ^ @\\0@"
                     , "\t deriving (Eq, Ord)"
+                    , ""
                     , "instance Show Token where"
-                    , '\t' :  intercalate "\n\t" (shows lexer)
+                    , '\t' : intercalate "\n\t" (shows lexer)
+                    , "\tshow TEoF = \"EoF\""
                     , ""
                 ]
 
 makeLexer :: LexerData -> GeneratedLexer
-makeLexer d = foldl (<>) $ makeLexer' <$> fromList d
+makeLexer d = foldl (<>) mempty $ makeLexer' <$> toList d
     where
-        makeLexer' :: (Text, TokenData)
+        makeLexer' :: (String, TokenData) -> GeneratedLexer
         makeLexer' (name, d) = GeneratedLexer {
-            captureLines = (quotify d) ++ " { mkL L" ++ name ++ " }"
-            , classes = 'L' : name
-            , classToTokens = 'L' : name ++ " -> return (T" ++ name ++ ' ' : (fromMaybeEmpty (tokenValue d)) ++ " p)"
-            , tokens = 'T' : name ++ "{ " ++ fromMaybeEmpty ((++ ",") <$> (extraFields d)) ++ "position :: AlexPosn } -- ^ " fromMaybe ('@' : (toLiteral d) ++ "@") (comment d)
-            , shows = "show (T" ++ name ++ ' ' : (fromMaybeEmpty showExtraFields) ++ " _) = " ++ fromMaybe (toLiteral d) (showBody d)
+            captureLines = [ (quotify d) ++ " { mkL L" ++ name ++ " }" ]
+            , classes = [ 'L' : name ]
+            , classToTokens = [ 'L' : name ++ " -> return (T" ++ name ++ ' ' : (fromMaybeEmpty (tokenValue d)) ++ " p)" ]
+            , tokens = [ 'T' : name ++ " { " ++ fromMaybeEmpty ((++ ",") <$> (extraFields d)) ++ "position :: AlexPosn } -- ^ " ++ fromMaybe ('@' : (toLiteral d) ++ "@") (comment d) ]
+            , shows = [ "show (T" ++ name ++ ' ' : (fromMaybeEmpty $ showExtraFields d) ++ " _) = " ++ fromMaybe ('"' : (toLiteral d) ++ "\"") (showBody d) ]
         }
             where
                 toLiteral d = fromMaybe (lexeme d) (literal d)
@@ -90,11 +97,11 @@ makeLexer d = foldl (<>) $ makeLexer' <$> fromList d
 
 
 data GeneratedLexer = GeneratedLexer {
-    captureLines :: [Text],
-    classes :: [Text],
-    classToTokens :: [Text],
-    tokens :: [Text],
-    shows :: [Text]
+    captureLines :: [String],
+    classes :: [String],
+    classToTokens :: [String],
+    tokens :: [String],
+    shows :: [String]
 }
 
 instance Monoid GeneratedLexer where
