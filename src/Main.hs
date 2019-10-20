a6fd70600f549e88021dc53453fc7424ac8ab5e0
Ed Jones <edjones98@gmail.com>
2019-10-20 19:25:09 +0100

    Added documentation

--- src/Main.hs
+++ src/Main.hs
@@ -1,2 +1,15 @@
+{-|
+Module      : Main
+Description : Entry-point for dig, compiler for Spade
+Copyright   : (c) Josh Findon and Edward Jones, 2019
+License     : MIT
+Maintainer  : Josh Findon and Edward Jones
+Stability   : experimental
+Portability : POSIX + Windows
+Language    : Haskell2010
+
+This module provides a CLI for dig.
+-}
+
 main :: IO ()
 main = putStrLn "Hello, world!"
