6f49f48a175e94eb8bff7ce721bfd0aa3b627584
Ed Jones <edjones98@gmail.com>
2019-10-21 12:29:28 +0100

    Added basic I/O and refactored compiler

--- src/Main.hs
+++ src/Main.hs
@@ -11,14 +11,38 @@ Language    : Haskell2010
 This module provides a CLI for dig.
 -}
 
-import           Modules.Results (Result (..))
+import           Generator.Generator         (generate)
+import           Language.SpadeParserWrapper (parse)
+import           Modules.Results             (Result (..))
+import           Optimiser.Optimiser         (optimise)
+import           TypeChecker.TypeChecker     (typeCheck)
 
 main :: IO ()
-main = putStrLn "Hello, world!"
+main = do
+    -- Fetch the input
+    c <- if True then
+            getContents
+        else
+            readFile "asdf.sp"
+    -- Compile the code
+    let rs = compile c
 
-compile :: String -> IO (Result [(String,String)])
+    -- Output
+    case rs of
+        Pass fs -> if True then
+                print rs
+            else
+                writeFile "asdf.mcfunction"
+        Fail es -> printErrors es
+
+compile :: String -> Result [(String,String)]
 compile = do
-    a <- parse "-"
     checkTypes a
     optimise
     generate
+
+printErrors :: Errors -> IO ()
+printErrors [] = return ()
+printErrors (e:es) = do
+    print e
+    printErrors es
